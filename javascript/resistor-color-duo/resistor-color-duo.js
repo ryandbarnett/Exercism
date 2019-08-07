768679720b64e63ab217e5e6b76f097ab5cee0c8
RyanDBarnett <conner2468@gmail.com>
2019-08-07 14:11:18 -0600

    Make basic solution to get first test to pass

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -3,6 +3,13 @@
 // convenience to get you started writing code faster.
 //
 
-export const value = () => {
-  
+export const value = (colors) => {
+  const COLORS = ['black', 'brown'];
+  let value = 0;
+  if (colors[0] !== 'black') {
+    value += COLORS.indexOf(colors[0]) * 10;
+  }
+  value += COLORS.indexOf(colors[1]);
+  return value;
+
 };
