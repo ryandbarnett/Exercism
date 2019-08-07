24988c7c857985537c608d4f3d94de32e1e40eb8
RyanDBarnett <conner2468@gmail.com>
2019-08-07 15:39:56 -0600

    Finish basic solution for space age

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -11,6 +11,24 @@ export const age = (planet, age_seconds) => {
     case 'mercury':
       return Number((age_seconds / (31557600 * 0.2408467)).toFixed(2));
       break;
+    case 'venus':
+      return Number((age_seconds / (31557600 * 0.61519726)).toFixed(2));
+      break;
+    case 'mars':
+      return Number((age_seconds / (31557600 * 1.8808158)).toFixed(2));
+      break;
+    case 'jupiter':
+      return Number((age_seconds / (31557600 * 11.862615)).toFixed(2));
+      break;
+    case 'saturn':
+      return Number((age_seconds / (31557600 * 29.447498)).toFixed(2));
+      break;
+    case 'uranus':
+      return Number((age_seconds / (31557600 * 84.016846)).toFixed(2));
+      break;
+    case 'neptune':
+      return Number((age_seconds / (31557600 * 164.79132)).toFixed(2));
+      break;
     default:
       return 'error';
   }
