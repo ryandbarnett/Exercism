606d1b4ffb0ac51978d6642521a1b15dc5a13280
RyanDBarnett <conner2468@gmail.com>
2019-08-10 09:03:53 -0600

    Finish collatz exercism

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -3,6 +3,10 @@
 // convenience to get you started writing code faster.
 //
 
-export const steps = () => {
-  throw new Error("Remove this statement and implement this function");
+export const steps = (n, count = 0) => {
+  if (n <= 0) throw new Error('Only positive numbers are allowed');
+  if (n === 1) return count;
+
+  n = n % 2 ? 3 * n + 1 : n / 2;
+  return steps(n, count += 1)
 };
