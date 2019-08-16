fb9e6a50fdba20b0241faeb2f2e820b0fcfdb6a7
RyanDBarnett <conner2468@gmail.com>
2019-08-16 11:28:06 -0600

    Finish hamming exercism

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -3,6 +3,27 @@
 // convenience to get you started writing code faster.
 //
 
-export const compute = () => {
-  throw new Error("Remove this statement and implement this function");
+export const compute = (strandOne, strandTwo) => {
+  const oneChars = strandOne.split('');
+  const twoChars = strandTwo.split('');
+
+  if (oneChars.length === 0 && twoChars.length !== 0) {
+    throw 'left strand must not be empty';
+  }
+
+  if (twoChars.length === 0 && oneChars.length !== 0) {
+    throw 'right strand must not be empty';
+  }
+
+  if (oneChars.length > twoChars.length || twoChars.length > oneChars.length) {
+    throw 'left and right strands must be of equal length';
+  }
+
+  let count = 0;
+
+  oneChars.forEach((char, i) => {
+    if (twoChars[i] !== char) count++;
+  })
+  
+  return count;
 };
