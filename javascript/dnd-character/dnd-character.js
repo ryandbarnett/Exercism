c24470572cdba1abc2a141bb0f9274de6bc4a399
Ryan Barnett <ryanbarnett883@gmail.com>
2019-08-21 15:25:08 -0600

    Changed author to Ryan

--- javascript/dnd-character/dnd-character.js
+++ javascript/dnd-character/dnd-character.js
@@ -3,40 +3,61 @@
 // convenience to get you started writing code faster.
 //
 
-export const abilityModifier = () => {
-  throw new Error("Remove this statement and implement this function");
+const rollD6 = () => {
+  return Math.floor((Math.random() * 6) + 1);
+}
+
+export const abilityModifier = (n) => {
+  if (n < 3) {
+    throw 'Ability scores must be at least 3';
+  }
+  if (n > 18) {
+    throw 'Ability scores can be at most 18';
+  }
+  const scores = {3: -4, 4: -3, 5: -3, 6: -2, 7: -2, 8: -1, 9: -1, 10: 0, 11: 0, 12: 1, 13: 1, 14: 2, 15: 2, 16: 3, 17: 3, 18: 4}
+  return scores[n];
 };
 
 export class Character {
+  constructor() {
+    this.strStat = Character.rollAbility();
+    this.dexStat = Character.rollAbility();
+    this.conStat = Character.rollAbility();
+    this.intStat = Character.rollAbility();
+    this.wisStat = Character.rollAbility();
+    this.charStat = Character.rollAbility();
+  }
+
   static rollAbility() {
-    throw new Error("Remove this statement and implement this function");
+    const rolls = [rollD6(), rollD6(), rollD6(), rollD6()];
+    return rolls.sort().slice(-3).reduce((acc, curRoll) => acc += curRoll);
   }
 
   get strength() {
-    throw new Error("Remove this statement and implement this function");
+    return this.strStat;
   }
 
   get dexterity() {
-    throw new Error("Remove this statement and implement this function");
+    return this.dexStat;
   }
 
   get constitution() {
-    throw new Error("Remove this statement and implement this function");
+    return this.conStat;
   }
 
   get intelligence() {
-    throw new Error("Remove this statement and implement this function");
+    return this.intStat;
   }
 
   get wisdom() {
-    throw new Error("Remove this statement and implement this function");
+    return this.wisStat;
   }
 
   get charisma() {
-    throw new Error("Remove this statement and implement this function");
+    return this.charStat;
   }
 
   get hitpoints() {
-    throw new Error("Remove this statement and implement this function");
+    return 10 + abilityModifier(this.conStat)
   }
 }
