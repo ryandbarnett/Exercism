581bac9672b548d1ad51608aef31d94c729f4a9c
RyanDBarnett <conner2468@gmail.com>
2019-08-23 10:38:32 -0600

    Finish perfect numbers exercism

--- javascript/perfect-numbers/perfect-numbers.js
+++ javascript/perfect-numbers/perfect-numbers.js
@@ -3,6 +3,24 @@
 // convenience to get you started writing code faster.
 //
 
-export const classify = () => {
-  
+export const classify = (n) => {
+  if (n <= 0) {
+    throw 'Classification is only possible for natural numbers.';
+  }
+
+  let sumOfFactors = 0;
+
+  for (let i = 1; i < n; i++) {
+    if (n % i === 0) {
+      sumOfFactors += i;
+    }
+  }
+
+  if (sumOfFactors === n) {
+    return 'perfect';
+  } else if (sumOfFactors > n) {
+    return 'abundant';
+  } else {
+    return 'deficient';
+  }
 };
