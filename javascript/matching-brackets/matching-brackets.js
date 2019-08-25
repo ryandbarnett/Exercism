cf7953821e694ebf6c9b021babcbbd4ef4f26fb7
RyanDBarnett <conner2468@gmail.com>
2019-08-25 11:54:38 -0600

    Finish matching brackets exercism

--- javascript/matching-brackets/matching-brackets.js
+++ javascript/matching-brackets/matching-brackets.js
@@ -3,6 +3,22 @@
 // convenience to get you started writing code faster.
 //
 
-export const matchingBrackets = () => {
+export const matchingBrackets = (brackets) => {
+  const reverse = {
+    '{': '}',
+    '[': ']',
+    '(': ')'
+  }
+
+  const wrongOrder = ['}', ')', ']']
   
+  if (brackets.length === 0) return true;
+  if (wrongOrder.includes(brackets[0])) return false;
+  const isMatch = brackets.indexOf(reverse[brackets[0]]);
+  if (isMatch !== -1 && isMatch % 2 === 1) {
+    brackets = brackets.split('').splice(isMatch, 1).splice(1);
+    return matchingBrackets(brackets)
+  } else {
+    return false;
+  }
 };
--- javascript/matching-brackets/matching-brackets.spec.js
+++ javascript/matching-brackets/matching-brackets.spec.js
@@ -5,31 +5,31 @@ describe('bracket push', () => {
     expect(matchingBrackets('{}')).toEqual(true);
   });
 
-  xtest('returns false for unclosed brackets', () => {
+  test('returns false for unclosed brackets', () => {
     expect(matchingBrackets('{{')).toEqual(false);
   });
 
-  xtest('returns false if brackets are out of order', () => {
+  test('returns false if brackets are out of order', () => {
     expect(matchingBrackets('}{')).toEqual(false);
   });
 
-  xtest('checks bracketing in more than one pair of brackets', () => {
+  test('checks bracketing in more than one pair of brackets', () => {
     expect(matchingBrackets('{}[]')).toEqual(true);
   });
 
-  xtest('checks bracketing in nested brackets', () => {
+  test('checks bracketing in nested brackets', () => {
     expect(matchingBrackets('{[]}')).toEqual(true);
   });
 
-  xtest('rejects brackets that are properly balanced but improperly nested', () => {
+  test('rejects brackets that are properly balanced but improperly nested', () => {
     expect(matchingBrackets('{[}]')).toEqual(false);
   });
 
-  xtest('checks bracket closure with deeper nesting', () => {
+  test('checks bracket closure with deeper nesting', () => {
     expect(matchingBrackets('{[)][]}')).toEqual(false);
   });
 
-  xtest('checks bracket closure in a long string of brackets', () => {
+  test('checks bracket closure in a long string of brackets', () => {
     expect(matchingBrackets('{[]([()])}')).toEqual(true);
   });
 });
