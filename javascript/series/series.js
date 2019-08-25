7443912c2ccf800d0cdfc9523debf71c3776641d
RyanDBarnett <conner2468@gmail.com>
2019-08-25 12:06:01 -0600

    Start series exercism

---
+++ javascript/series/series.js
@@ -0,0 +1,18 @@
+//
+// This is only a SKELETON file for the 'Series' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Series {
+  constructor() {
+    
+  }
+
+  get digits() {
+    
+  }
+
+  slices() {
+    
+  }
+}
