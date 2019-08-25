7b5031dbed1178c4a7fe8ca5aa055473b58a8d60
RyanDBarnett <conner2468@gmail.com>
2019-08-25 17:04:35 -0600

    Refactor protein-translation

--- javascript/protein-translation/protein-translation.js
+++ javascript/protein-translation/protein-translation.js
@@ -3,6 +3,33 @@
 // convenience to get you started writing code faster.
 //
 
-export const translate = () => {
-  
+export const translate = (rna) => {
+  const sequences = {
+    "AUG": "Methionine",
+    "UUU": "Phenylalanine",
+    "UUC": "Phenylalanine",
+    "UUA": "Leucine",
+    "UUG": "Leucine",
+    "UCU": "Serine",
+    "UCC": "Serine",
+    "UCA": "Serine",
+    "UCG": "Serine",
+    "UAU": "Tyrosine",
+    "UAC": "Tyrosine",
+    "UGU": "Cysteine",
+    "UGC": "Cysteine",
+    "UGG": "Tryptophan",
+    "UAA": "STOP",
+    "UAG": "STOP",
+    "UGA": "STOP"
+  }
+  if (!rna) return [];
+  rna = rna.match(new RegExp('.{1,3}', 'g'));
+  const translated = rna.map(sequence => {
+    if (!sequences[sequence]) throw 'Invalid codon';
+    return sequences[sequence]
+  });
+  const hasStop = translated.indexOf("STOP") !== -1;
+  if (hasStop) translated.splice(translated.indexOf("STOP"));
+  return translated;
 };
