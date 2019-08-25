b9dc66d5198a47c89b86c6f4bd3a8ae0f16097cf
RyanDBarnett <conner2468@gmail.com>
2019-08-25 14:25:26 -0600

    Finish transpose exercism

--- javascript/transpose/transpose.js
+++ javascript/transpose/transpose.js
@@ -3,6 +3,30 @@
 // convenience to get you started writing code faster.
 //
 
-export const transpose = () => {
+export const transpose = (input) => {
+  let result = [];
+  const lengths = input.map(word => word.length);
+  const longest = lengths.sort()[lengths.length - 1];
+  const indexOfLongest = lengths.indexOf(longest);
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < longest; j++) {
+      if (!result[j]) result[j] = [];
+
+      if (!input[i][j]) {
+        if (indexOfLongest > i) {
+          result[j] = result[j] + ' ';
+        } else {
+          result[j] = result[j] + '';
+        }
+      } else {
+        result[j] = result[j] + input[i][j];
+      }
+    }
+  }
+
+  if (result[result.length - 1]) {
+    result[result.length - 1] = result[result.length - 1].trimEnd();
+  }
   
+  return result;
 };
