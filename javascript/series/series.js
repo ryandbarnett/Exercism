837aa0b4f4c7f161a17d6d80c85ef7acb892bb6c
RyanDBarnett <conner2468@gmail.com>
2019-08-25 12:31:06 -0600

    Finished series exercism

--- javascript/series/series.js
+++ javascript/series/series.js
@@ -4,15 +4,22 @@
 //
 
 export class Series {
-  constructor() {
-    
+  constructor(str) {
+    this.str = str;
   }
 
   get digits() {
-    
+    return this.str.split('').map(Number);
   }
 
-  slices() {
-    
+  slices(n) {
+    if (n > this.digits.length) throw 'Slice size is too big.';
+
+    return this.digits.reduce((acc, cur, i) => {
+      if (this.digits.slice(i, i + n).length === n) {
+        acc.push(this.digits.slice(i, i + n));
+      }
+      return acc;
+    }, [])
   }
 }
