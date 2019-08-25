d92e3c7d8294f1687a95f0aa5f80af84a29ee142
RyanDBarnett <conner2468@gmail.com>
2019-08-25 17:42:24 -0600

    Start darts exercism

---
+++ javascript/darts/darts.js
@@ -0,0 +1,10 @@
+//
+// This is only a SKELETON file for the 'Darts' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const solve = (x, y) => {
+  // points 0 1 5 10
+  // radii  > 10 5 1
+  console.log(Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)))
+};
