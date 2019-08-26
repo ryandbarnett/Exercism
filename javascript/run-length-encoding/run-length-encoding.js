6ad0b2eea888cf649410b5f3004dadfb87f06293
RyanDBarnett <conner2468@gmail.com>
2019-08-25 18:55:02 -0600

    Start run length encoding exercism

---
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -0,0 +1,12 @@
+//
+// This is only a SKELETON file for the 'Run Length Encoding' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const encode = () => {
+  
+};
+
+export const decode = () => {
+  
+};
