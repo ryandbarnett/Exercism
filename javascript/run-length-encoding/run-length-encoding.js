7aa9de2f04a2394aad6184a7ae20ff978897b34c
RyanDBarnett <conner2468@gmail.com>
2019-08-25 22:35:49 -0600

    Finish run length encoding exercism

--- javascript/run-length-encoding/run-length-encoding.js
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -3,10 +3,35 @@
 // convenience to get you started writing code faster.
 //
 
-export const encode = () => {
-  
+export const encode = (str) => {
+  if (!str.length) return '';
+  let count = 1;
+  return str.split('').reduce((acc, curChar, i) => {
+    if (curChar === str[i + 1]) {
+      count++;
+    } else {
+      if (count === 1) {
+        acc += curChar;
+      } else {
+        acc += `${count}${curChar}`
+      }
+      count = 1;
+    }
+    return acc;
+  }, '');
 };
 
-export const decode = () => {
-  
+export const decode = (str) => {
+  return str.split('').reduce((acc, curChar, i, arr) => {
+    if (parseInt(curChar)) {
+      return acc;
+    } else if (parseInt(arr[i - 1]) && parseInt(arr[i - 2])) {
+      acc += curChar.repeat(parseInt(arr[i - 2] + arr[i - 1]))
+    } else if (parseInt(arr[i - 1])) {
+      acc += curChar.repeat(parseInt(arr[i - 1]))
+    } else {
+      acc += curChar;
+    }
+    return acc;
+  }, '');
 };
