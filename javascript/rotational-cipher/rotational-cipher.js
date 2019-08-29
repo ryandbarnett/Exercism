1bd2f0e67feace2529ce2f3f44bb7385d44d85a5
RyanDBarnett <conner2468@gmail.com>
2019-08-29 04:52:30 -0600

    Finish rotational cipher exercism

--- javascript/rotational-cipher/rotational-cipher.js
+++ javascript/rotational-cipher/rotational-cipher.js
@@ -6,7 +6,32 @@
 export class RotationalCipher {
   static rotate(chars, num) {
     return chars.split('').map((char, index) => {
-      return String.fromCharCode(chars.charCodeAt(index) + num);
+      return rotateCharacter(char, num);
     }).join('');
   }
+}
+
+const rotateCharacter = (char, num) => {
+  const oldCode = char.charCodeAt(0);
+  const newCode = oldCode + num;
+  const isCapital = oldCode >= 65 && oldCode <= 90;
+  const isLower = oldCode >= 97 && oldCode <= 122;
+  
+  if (!isCapital && !isLower) {
+    return char;
+  }
+
+  if (isCapital && newCode > 90) {
+    return parseCode(65 + newCode % 91);
+  }
+
+  if (isLower && newCode > 122) {
+    return parseCode(97 + newCode % 123);
+  }
+
+  return parseCode(char.charCodeAt(0) + num);
+}
+
+const parseCode = (num) => {
+  return String.fromCharCode(num);
 }
