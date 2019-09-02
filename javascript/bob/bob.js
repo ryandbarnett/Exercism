1658a1d1a87b30cccf5ae091abd873f5643eec34
RyanDBarnett <conner2468@gmail.com>
2019-09-02 17:51:26 -0600

    Finished Bob exercism

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -5,7 +5,18 @@
 //
 
 export const hey = (message) => {
-  //
-  // YOUR CODE GOES HERE
-  //
+  message = message.replace(/[ \t\r\n]/g, '');
+  const isQuestion = /\?$/.test(message);
+  if (!message) return 'Fine. Be that way!';
+  message = message.replace(/[^a-zA-Z]/g, '');
+  const isYelling = /^[A-Z]+$/.test(message);
+  const isForcefulQuestion = isQuestion && isYelling;
+  if (isForcefulQuestion) {
+    return 'Calm down, I know what I\'m doing!';
+  } else if (isYelling) {
+    return 'Whoa, chill out!';
+  } else if (isQuestion) {
+    return 'Sure.';
+  } 
+  return 'Whatever.';
 };
