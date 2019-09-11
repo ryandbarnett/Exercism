5efcc80bd72ea95a262e23f0ad31b362d810b429
RyanDBarnett <conner2468@gmail.com>
2019-09-11 11:38:11 -0600

    Start linked list exercism

---
+++ javascript/linked-list/linked-list.js
@@ -0,0 +1,30 @@
+//
+// This is only a SKELETON file for the 'Linked List' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class LinkedList {
+  constructor() {
+    this.list = [];
+  }
+
+  push(n) {
+    return this.list.push(n);
+  }
+
+  pop() {
+    return this.list.pop();
+  }
+
+  shift() {
+  }
+
+  unshift() {
+  }
+
+  delete() {
+  }
+
+  count() {
+  }
+}
