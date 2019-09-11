75ce8e8f1b9f6f5bad95efbdb893384e3c421905
RyanDBarnett <conner2468@gmail.com>
2019-09-11 13:16:33 -0600

    Finish grade school exercism

--- javascript/grade-school/grade-school.js
+++ javascript/grade-school/grade-school.js
@@ -4,12 +4,24 @@
 //
 
 export class GradeSchool {
+  constructor() {
+    this.list = {};
+  }
+
   roster() {
+    return Object.keys(this.list).reduce((acc, key) => {
+      acc[key] = [...this.list[key]];
+      return acc;
+    }, {});
   }
 
-  add() {
+  add(name, grade) {
+    if (!this.list[grade]) this.list[grade] = [];
+    this.list[grade].push(name);
+    this.list[grade].sort();
   }
 
-  grade() {
+  grade(n) {
+    return this.list[n] ? [...this.list[n]] : [];
   }
 }
