123fc5b395f0fe7ae67a21677306b12ff3b54cfe
RyanDBarnett <conner2468@gmail.com>
2019-09-11 12:03:54 -0600

    Start grade school exercism

---
+++ javascript/grade-school/grade-school.js
@@ -0,0 +1,15 @@
+//
+// This is only a SKELETON file for the 'Grade School' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class GradeSchool {
+  roster() {
+  }
+
+  add() {
+  }
+
+  grade() {
+  }
+}
